594702018d86aa0606c1d266be506b7c99bb9c8a
Cristopher Castellon Guillen <cristophercastellon0@gmail.com>
2026-09-01 15:55:41 -0600

    v1

--- Block de Notas VB.NET/Form1.Designer.vb
+++ Block de Notas VB.NET/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
-Partial Class Form1
+Partial Class frmBlocNotas
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,10 +22,202 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        components = New ComponentModel.Container()
+        MnuPrincipal = New MenuStrip()
+        tsPrincipal = New ToolStrip()
+        rtbDocumento = New RichTextBox()
+        cmsTexto = New ContextMenuStrip(components)
+        CortarToolStripMenuItem = New ToolStripMenuItem()
+        PegarToolStripMenuItem = New ToolStripMenuItem()
+        CopiarToolStripMenuItem = New ToolStripMenuItem()
+        SelecionarTodoToolStripMenuItem = New ToolStripMenuItem()
+        FuentesToolStripMenuItem = New ToolStripMenuItem()
+        stsInferior = New StatusStrip()
+        dlgAbrir = New OpenFileDialog()
+        dlgGuardar = New SaveFileDialog()
+        dlgFuentes = New FontDialog()
+        dlgColor = New ColorDialog()
+        tmrReloj = New Timer(components)
+        AyudaToolStripMenuItem = New ToolStripMenuItem()
+        EdicionToolStripMenuItem = New ToolStripMenuItem()
+        FormatoToolStripMenuItem = New ToolStripMenuItem()
+        ArchivoToolStripMenuItem = New ToolStripMenuItem()
+        mnuNuevo = New ToolStripMenuItem()
+        MnuPrincipal.SuspendLayout()
+        cmsTexto.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' MnuPrincipal
+        ' 
+        MnuPrincipal.ImageScalingSize = New Size(20, 20)
+        MnuPrincipal.Items.AddRange(New ToolStripItem() {ArchivoToolStripMenuItem, FormatoToolStripMenuItem, EdicionToolStripMenuItem, AyudaToolStripMenuItem})
+        MnuPrincipal.Location = New Point(0, 0)
+        MnuPrincipal.Name = "MnuPrincipal"
+        MnuPrincipal.Size = New Size(800, 28)
+        MnuPrincipal.TabIndex = 0
+        MnuPrincipal.Text = "MenuStrip1"
+        ' 
+        ' tsPrincipal
+        ' 
+        tsPrincipal.GripStyle = ToolStripGripStyle.Hidden
+        tsPrincipal.ImageScalingSize = New Size(20, 20)
+        tsPrincipal.Location = New Point(0, 28)
+        tsPrincipal.Name = "tsPrincipal"
+        tsPrincipal.Size = New Size(800, 25)
+        tsPrincipal.TabIndex = 1
+        tsPrincipal.Text = "ToolStrip1"
+        ' 
+        ' rtbDocumento
+        ' 
+        rtbDocumento.ContextMenuStrip = cmsTexto
+        rtbDocumento.Dock = DockStyle.Fill
+        rtbDocumento.Font = New Font("Consolas", 11F)
+        rtbDocumento.Location = New Point(0, 53)
+        rtbDocumento.Name = "rtbDocumento"
+        rtbDocumento.Size = New Size(800, 397)
+        rtbDocumento.TabIndex = 2
+        rtbDocumento.Text = ""
+        ' 
+        ' cmsTexto
+        ' 
+        cmsTexto.ImageScalingSize = New Size(20, 20)
+        cmsTexto.Items.AddRange(New ToolStripItem() {CortarToolStripMenuItem, PegarToolStripMenuItem, CopiarToolStripMenuItem, SelecionarTodoToolStripMenuItem, FuentesToolStripMenuItem})
+        cmsTexto.Name = "cmsTexto"
+        cmsTexto.Size = New Size(186, 124)
+        ' 
+        ' CortarToolStripMenuItem
+        ' 
+        CortarToolStripMenuItem.Name = "CortarToolStripMenuItem"
+        CortarToolStripMenuItem.Size = New Size(185, 24)
+        CortarToolStripMenuItem.Text = "Cortar"
+        ' 
+        ' PegarToolStripMenuItem
+        ' 
+        PegarToolStripMenuItem.Name = "PegarToolStripMenuItem"
+        PegarToolStripMenuItem.Size = New Size(185, 24)
+        PegarToolStripMenuItem.Text = "Pegar"
+        ' 
+        ' CopiarToolStripMenuItem
+        ' 
+        CopiarToolStripMenuItem.Name = "CopiarToolStripMenuItem"
+        CopiarToolStripMenuItem.Size = New Size(185, 24)
+        CopiarToolStripMenuItem.Text = "Copiar"
+        ' 
+        ' SelecionarTodoToolStripMenuItem
+        ' 
+        SelecionarTodoToolStripMenuItem.Name = "SelecionarTodoToolStripMenuItem"
+        SelecionarTodoToolStripMenuItem.Size = New Size(185, 24)
+        SelecionarTodoToolStripMenuItem.Text = "Selecionar Todo"
+        ' 
+        ' FuentesToolStripMenuItem
+        ' 
+        FuentesToolStripMenuItem.Name = "FuentesToolStripMenuItem"
+        FuentesToolStripMenuItem.Size = New Size(185, 24)
+        FuentesToolStripMenuItem.Text = "Fuentes"
+        ' 
+        ' stsInferior
+        ' 
+        stsInferior.ImageScalingSize = New Size(20, 20)
+        stsInferior.Location = New Point(0, 428)
+        stsInferior.Name = "stsInferior"
+        stsInferior.Size = New Size(800, 22)
+        stsInferior.TabIndex = 4
+        stsInferior.Text = "StatusStrip1"
+        ' 
+        ' dlgAbrir
+        ' 
+        dlgAbrir.FileName = "OpenFileDialog1"
+        dlgAbrir.Filter = "Archivos de texto (*.txt)|*.txt|Todos (*.*)|*.*"
+        ' 
+        ' dlgGuardar
+        ' 
+        dlgGuardar.DefaultExt = "txt"
+        ' 
+        ' dlgFuentes
+        ' 
+        dlgFuentes.ShowColor = True
+        ' 
+        ' dlgColor
+        ' 
+        dlgColor.FullOpen = True
+        ' 
+        ' tmrReloj
+        ' 
+        tmrReloj.Enabled = True
+        tmrReloj.Interval = 1000
+        ' 
+        ' AyudaToolStripMenuItem
+        ' 
+        AyudaToolStripMenuItem.Name = "AyudaToolStripMenuItem"
+        AyudaToolStripMenuItem.Size = New Size(65, 24)
+        AyudaToolStripMenuItem.Text = "Ayuda"
+        ' 
+        ' EdicionToolStripMenuItem
+        ' 
+        EdicionToolStripMenuItem.Name = "EdicionToolStripMenuItem"
+        EdicionToolStripMenuItem.Size = New Size(72, 24)
+        EdicionToolStripMenuItem.Text = "Edicion"
+        ' 
+        ' FormatoToolStripMenuItem
+        ' 
+        FormatoToolStripMenuItem.Name = "FormatoToolStripMenuItem"
+        FormatoToolStripMenuItem.Size = New Size(79, 24)
+        FormatoToolStripMenuItem.Text = "Formato"
+        ' 
+        ' ArchivoToolStripMenuItem
+        ' 
+        ArchivoToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {mnuNuevo})
+        ArchivoToolStripMenuItem.Name = "ArchivoToolStripMenuItem"
+        ArchivoToolStripMenuItem.Size = New Size(73, 24)
+        ArchivoToolStripMenuItem.Text = "Archivo"
+        ' 
+        ' mnuNuevo
+        ' 
+        mnuNuevo.Name = "mnuNuevo"
+        mnuNuevo.ShortcutKeys = Keys.Control Or Keys.N
+        mnuNuevo.Size = New Size(224, 26)
+        mnuNuevo.Text = "&Nuevo"
+        ' 
+        ' frmBlocNotas
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(800, 450)
-        Text = "Form1"
+        Controls.Add(stsInferior)
+        Controls.Add(rtbDocumento)
+        Controls.Add(tsPrincipal)
+        Controls.Add(MnuPrincipal)
+        MainMenuStrip = MnuPrincipal
+        MinimumSize = New Size(600, 400)
+        Name = "frmBlocNotas"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Bloc de Notas VB.NET"
+        MnuPrincipal.ResumeLayout(False)
+        MnuPrincipal.PerformLayout()
+        cmsTexto.ResumeLayout(False)
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents MnuPrincipal As MenuStrip
+    Friend WithEvents tsPrincipal As ToolStrip
+    Friend WithEvents rtbDocumento As RichTextBox
+    Friend WithEvents cmsTexto As ContextMenuStrip
+    Friend WithEvents stsInferior As StatusStrip
+    Friend WithEvents dlgAbrir As OpenFileDialog
+    Friend WithEvents dlgGuardar As SaveFileDialog
+    Friend WithEvents CortarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PegarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CopiarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SelecionarTodoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FuentesToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents dlgFuentes As FontDialog
+    Friend WithEvents dlgColor As ColorDialog
+    Friend WithEvents tmrReloj As Timer
+    Friend WithEvents ArchivoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FormatoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EdicionToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AyudaToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents mnuNuevo As ToolStripMenuItem
 
 End Class
--- Block de Notas VB.NET/Form1.vb
+++ Block de Notas VB.NET/Form1.vb
@@ -1,3 +1,9 @@
-﻿Public Class Form1
+﻿Public Class frmBlocNotas
+    Private Sub rtbDocumento_TextChanged(sender As Object, e As EventArgs) Handles rtbDocumento.TextChanged
 
+    End Sub
+
+    Private Sub MnuPrincipal_ItemClicked(sender As Object, e As ToolStripItemClickedEventArgs) Handles MnuPrincipal.ItemClicked
+
+    End Sub
 End Class
